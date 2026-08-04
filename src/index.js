c56605a314f7c06527659fa2a42dd151f038aecb
skorpio <rakesh48bk@gmail.com>
2026-08-04 12:26:26 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ dotenv.config({
 });
 console.log("URI:", process.env.MONGODB_URI);
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`Server running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("DB connection failed : ",err);
+})
 
 
 
